8b43916251063e58f82bf3f59ea708553471b200
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-10-14 20:53:29 +0200

    Adding Product class

---
+++ Shopinka/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopinka.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUri { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
