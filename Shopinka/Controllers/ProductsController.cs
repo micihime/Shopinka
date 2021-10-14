75cb90da09a1779b968d2f6cfeee13ed140560ab
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-10-14 20:36:05 +0200

    Removing default model and controller created from project template, adding dummy Product controller and setting the app to launch it

---
+++ Shopinka/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Shopinka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+    }
+}
