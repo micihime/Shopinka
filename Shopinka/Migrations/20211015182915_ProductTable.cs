0273bf28cf919654c925d8b9f5759502574739c9
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-10-15 20:29:56 +0200

    Adding data annotations to Product;s properties

---
+++ Shopinka/Migrations/20211015182915_ProductTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shopinka.Migrations
+{
+    public partial class ProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUri",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUri",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Shopinka/Models/Product.cs
+++ Shopinka/Models/Product.cs
@@ -1,11 +1,22 @@
-﻿namespace Shopinka.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopinka.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Required]
         public string ImageUri { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
         public string Description { get; set; }
     }
 }
