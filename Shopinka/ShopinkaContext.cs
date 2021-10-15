5caff319fe66c052f2b1bb55f87a5737c19d820e
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-10-15 20:24:03 +0200

    Installing EF Core, creating DbContext, adding first migration for creating the Products table

---
+++ Shopinka/ShopinkaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopinka.Models;
+
+namespace Shopinka
+{
+    public class ShopinkaContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=ShopinkaDb;Trusted_Connection=True;");
+        }
+    }
+}
