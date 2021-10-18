d98c1fb81b11b76b191999e44e5cc91079fa9c8a
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-10-18 22:10:42 +0200

    Implementing Product repo and service

---
+++ Shopinka.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Shopinka.Core.Repositories;
+using Shopinka.Models;
+
+namespace Shopinka.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ShopinkaContext context) : base(context) { }
+    }
+}
---
+++ Shopinka.Service/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using Shopinka.Core;
+using Shopinka.Core.Services;
+using Shopinka.Models;
+using System.Collections.Generic;
+
+namespace Shopinka.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _unitOfWork.Products.GetAll();
+        }
+
+        public Product GetById(int id)
+        {
+            return _unitOfWork.Products.GetById(id);
+        }
+
+        public void UpdateDesc(int id, string desc)
+        {
+            var product = _unitOfWork.Products.GetById(id);
+            product.Description = desc;
+            _unitOfWork.Commit();
+        }
+    }
+}
