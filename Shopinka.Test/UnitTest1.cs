bd5e0fd7475089eed4ba97a243f556baa65b0b30
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-12-20 15:11:18 +0100

    Product controller tests - get all items

--- Shopinka.Test/UnitTest1.cs
+++ Shopinka.Test/UnitTest1.cs
@@ -1,14 +1,41 @@
-using System;
+using Microsoft.AspNetCore.Mvc;
+using Shopinka.Controllers;
+using Shopinka.Core.Services;
+using Shopinka.Models;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Shopinka.Test
 {
+
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ProductsController _controller;
+        private readonly IProductService _service;
 
+        public UnitTest1()
+        {
+            _service = new ProductFakeService();
+            _controller = new ProductsController(_service);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.Get();
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.Get() as OkObjectResult;
+            // Assert
+            var items = Assert.IsType<List<Product>>(okResult.Value);
+            Assert.Equal(13, items.Count);
         }
     }
 }
