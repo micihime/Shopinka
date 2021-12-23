6da29986a1d513bd952de3e4d327993a7db4da05
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-12-23 10:56:13 +0100

    Using Moq in the unit tests

--- Shopinka.Test/ProductsControllerTests.cs
+++ Shopinka.Test/ProductsControllerTests.cs
@@ -1,9 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Shopinka.Api.Controllers;
 using Shopinka.Api.Dtos;
 using Shopinka.Core.Services;
 using Shopinka.Models;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Shopinka.Test
@@ -11,19 +13,167 @@ namespace Shopinka.Test
 
     public class ProductsControllerTests
     {
-        private readonly ProductsController _controller;
-        private readonly IProductService _service;
+        private readonly Mock<IProductService> _service;
 
         public ProductsControllerTests()
         {
-            _service = new ProductFakeService();
-            _controller = new ProductsController(_service);
+            _service = new Mock<IProductService>();
         }
 
+        #region CONSTANTS AS INPUTS TO METHODS
+
+        private const int testId = 1;
+        private const int notExistingTestId = 1000;
+        private const int pageSize = 5;
+        private const int pageNumber = 1;
+        private const int notExistingPageNumber = 30;
+        private const string testDesc = "test desc";
+        #endregion
+
+        #region SERVICE SETUP METHODS
+
+        private List<Product> getSampleProducts()
+        {
+            return new List<Product>()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Arónia osudu",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 1.99M,
+                    Description = "Vášeň ukrytá v čokoláde. Čokoláda je jedným z najväčších pokušení aké si človek len môže predstaviť. "
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Karamelový anjelik",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 0.69M,
+                    Description = "Čokoláda je jedným z najväčších pokušení aké si človek len môže predstaviť. "
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Rumová chrumka",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 2.69M,
+                    Description = "Chrumkavá vášeň ukrytá v čokoláde. Jemná príchuť kubánskeho rumu."
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "Jahodová chrumka",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 1.69M,
+                    Description = "Chrumkavá vášeň ukrytá v čokoláde. Dotyk jahôdok. "
+                },
+                new Product
+                {
+                    Id = 5,
+                    Name = "Čili chrumka",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 0.39M,
+                    Description = "Vášeň ukrytá v čokoláde. Okoreňte si svoj život. "
+                },
+                new Product
+                {
+                    Id = 6,
+                    Name = "Kávička v čokoláde",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 2.39M,
+                    Description = "Vášeň ukrytá v čokoláde. Espresso shot v čokoláde. "
+                },
+                new Product
+                {
+                    Id = 7,
+                    Name = "Double trouble",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 1.39M,
+                    Description = "Prekvapenie."
+                },
+                new Product
+                {
+                    Id = 8,
+                    Name = "Agent Provocateur",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 0.79M,
+                    Description = "Zážitok."
+                },
+                new Product
+                {
+                    Id = 9,
+                    Name = "Mandľové praliné",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 2.79M,
+                    Description = "Najpredávanejšia pralinka."
+                },
+                new Product
+                {
+                    Id = 10,
+                    Name = "Lieskovoorieškové praliné",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 1.79M,
+                    Description = "Luxusná variácia najpredávanejšej pralinky."
+                },
+                new Product
+                {
+                    Id = 11,
+                    Name = "Čože je to borovička",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 0.49M,
+                    Description = "Čokoládová ganache s dotykom najkvalitnejšej borovičky."
+                },
+                new Product
+                {
+                    Id = 12,
+                    Name = "Čože je to slivovička",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 2.49M,
+                    Description = "Čokoládová ganache s dotykom najkvalitnejšej slivovičky."
+                },
+                new Product
+                {
+                    Id = 13,
+                    Name = "Harmančekové pohladenie",
+                    ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                    Price = 1.49M,
+                    Description = "Herbal infusion. Upokojujúca chuť harmančeka v sladkom objatí bielej čokolády."
+                }
+            };
+        }
+
+        private List<Product> getPagedProducts()
+        {
+            return getSampleProducts().Take(pageSize).ToList();
+        }
+
+        private List<Product> getEmpty() { return new List<Product>(); }
+
+        private Product getProduct()
+        {
+            return new Product
+            {
+                Id = 1,
+                Name = "Arónia osudu",
+                ImageUri = "https://elezi.sk/wp-content/uploads/2019/08/photo_21-02-2019_14_18_01-upr_1.jpg",
+                Price = 1.99M,
+                Description = "Vášeň ukrytá v čokoláde. Čokoláda je jedným z najväčších pokušení aké si človek len môže predstaviť. "
+            };
+        }
+
+        private Product getNull() { return null; }
+        #endregion
+
         #region GET ALL
+
         [Fact]
         public void Get_WhenCalled_ReturnsOkResult()
         {
+            // Arrange
+            _service.Setup(x => x.GetAll())
+                .Returns(getSampleProducts);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get();
             // Assert
@@ -33,6 +183,10 @@ namespace Shopinka.Test
         [Fact]
         public void Get_WhenCalled_ReturnsAllItems()
         {
+            // Arrange
+            _service.Setup(x => x.GetAll())
+                .Returns(getSampleProducts);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get() as OkObjectResult;
             // Assert
@@ -42,11 +196,15 @@ namespace Shopinka.Test
         #endregion
 
         #region GET ALL (PAGED)
+
         [Fact]
         public void GetPaged_WhenCalled_ReturnsOkResult()
         {
             // Arrange
-            var dto = new PagingDto { PageNumber = 1, PageSize = 5 };
+            var dto = new PagingDto { PageNumber = pageNumber, PageSize = pageSize };
+            _service.Setup(x => x.GetAll(dto.PageNumber, dto.PageSize))
+                .Returns(getPagedProducts);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get(dto);
             // Assert
@@ -57,8 +215,10 @@ namespace Shopinka.Test
         public void GetPaged_WhenCalled_ReturnsRequestedNumberOfItems()
         {
             // Arrange
-            int pageSize = 5;
-            var dto = new PagingDto { PageNumber = 1, PageSize = pageSize };
+            var dto = new PagingDto { PageNumber = pageNumber, PageSize = pageSize };
+            _service.Setup(x => x.GetAll(dto.PageNumber, dto.PageSize))
+                .Returns(getPagedProducts);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get(dto) as OkObjectResult;
             // Assert
@@ -70,8 +230,10 @@ namespace Shopinka.Test
         public void GetPaged_NotExistingPage_ReturnsOkResult()
         {
             // Arrange
-            int pageNumber = 30;
-            var dto = new PagingDto { PageNumber = pageNumber, PageSize = 5 };
+            var dto = new PagingDto { PageNumber = notExistingPageNumber, PageSize = pageSize };
+            _service.Setup(x => x.GetAll(dto.PageNumber, dto.PageSize))
+                .Returns(getEmpty);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get(dto);
             // Assert
@@ -82,8 +244,10 @@ namespace Shopinka.Test
         public void GetPaged_NotExistingPage_ReturnsEmpty()
         {
             // Arrange
-            int pageNumber = 30;
-            var dto = new PagingDto { PageNumber = pageNumber, PageSize = 5 };
+            var dto = new PagingDto { PageNumber = notExistingPageNumber, PageSize = pageSize };
+            _service.Setup(x => x.GetAll(dto.PageNumber, dto.PageSize))
+                .Returns(getEmpty);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get(dto) as OkObjectResult;
             // Assert
@@ -93,13 +257,16 @@ namespace Shopinka.Test
         #endregion
 
         #region GET BY ID
+
         [Fact]
         public void GetById_UnknownIdPassed_ReturnsNotFoundResult()
         {
             // Arrange
-            var testId = 1000;
+            _service.Setup(x => x.GetById(notExistingTestId))
+               .Returns(getNull);
+            var _controller = new ProductsController(_service.Object);
             // Act
-            var notFoundResult = _controller.Get(testId);
+            var notFoundResult = _controller.Get(notExistingTestId);
             // Assert
             Assert.IsType<NotFoundResult>(notFoundResult);
         }
@@ -108,7 +275,9 @@ namespace Shopinka.Test
         public void GetById_ExistingIdPassed_ReturnsOkResult()
         {
             // Arrange
-            var testId = 1;
+            _service.Setup(x => x.GetById(testId))
+               .Returns(getProduct);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get(testId);
             // Assert
@@ -119,7 +288,9 @@ namespace Shopinka.Test
         public void GetById_ExistingIdPassed_ReturnsRightItem()
         {
             // Arrange
-            var testId = 1;
+            _service.Setup(x => x.GetById(testId))
+               .Returns(getProduct);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var okResult = _controller.Get(testId) as OkObjectResult;
             // Assert
@@ -129,15 +300,17 @@ namespace Shopinka.Test
         #endregion
 
         #region PUT
+
         [Fact]
         public void Put_UnknownIdPassed_ReturnsNotFoundResult()
         {
             // Arrange
-            var testId = 1000;
-            var testDesc = "test desc";
-            var dto = new ProductDto { Id = testId, Description = testDesc };
+            var dto = new ProductDto { Id = notExistingPageNumber, Description = testDesc };
+            _service.Setup(x => x.UpdateDesc(dto.Id, dto.Description))
+               .Returns(false);
+            var _controller = new ProductsController(_service.Object);
             // Act
-            var badReqResult = _controller.Put(testId, dto);
+            var badReqResult = _controller.Put(notExistingPageNumber, dto);
             // Assert
             Assert.IsType<BadRequestResult>(badReqResult as BadRequestResult);
         }
@@ -146,9 +319,10 @@ namespace Shopinka.Test
         public void Put_DifferrentIdInDto_ReturnsBadRequestResult()
         {
             // Arrange
-            var testId = 1;
-            var testDesc = "test desc";
-            var dto = new ProductDto { Id = testId++, Description = testDesc };
+            var dto = new ProductDto { Id = notExistingPageNumber, Description = testDesc };
+            //_service.Setup(x => x.UpdateDesc(dto.Id, dto.Description))
+            //   .Returns(false);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var badReqResult = _controller.Put(testId, dto);
             // Assert
@@ -159,9 +333,10 @@ namespace Shopinka.Test
         public void Put_ExistingIdPassed_ReturnsNoContentResult()
         {
             // Arrange
-            var testId = 1;
-            var testDesc = "test desc";
             var dto = new ProductDto { Id = testId, Description = testDesc };
+            _service.Setup(x => x.UpdateDesc(dto.Id, dto.Description))
+               .Returns(true);
+            var _controller = new ProductsController(_service.Object);
             // Act
             var noContentResult = _controller.Put(testId, dto);
             // Assert
